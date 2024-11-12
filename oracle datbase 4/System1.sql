c290e39b7dc886bb97f63cf90424ce613f01f245
Mirfak-per <ga.hernandezl@duocuc.cl>
2024-11-12 11:09:52 -0300

    Add files via upload

---
+++ oracle datbase 4/System1.sql
@@ -0,0 +1,6 @@
+alter session set "_oracle_script"=true;
+create user EJEMPLODML IDENTIFIED by EJEMPLODML;
+Grant all privileges to EJEMPLODML;
+
+create user RENTAHOUSE IDENTIFIED by RENTAHOUSE;
+Grant all privileges to RENTAHOUSE;
